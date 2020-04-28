7ffd0f4dd57ad03093398181085ebf54050e0527
banana-galaxy <cawasp@gmail.com>
2020-04-28 13:25:50 -0700

    added blocks

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame, pyautogui
+import pygame, pyautogui, random
 
 class Blob():
     def __init__(self, windowSize):
@@ -12,6 +12,7 @@ class Blob():
         self.jumpVel = 20
         self.fallVel = 1
         self.blobShow = 1
+        self.onGround = True
 
     def changeDraw(self):
         if self.blobShow:
@@ -27,9 +28,13 @@ class Blob():
 
     def getPos(self):
         return self.pos
+
+    def getSize(self):
+        return self.blobSize
     
     def move(self, y):
         obstacle = False
+        self.onGround = False
         if y < 0 and self.collision == -1:
             obstacle = True
         if y > 0 and self.collision == 1:
@@ -37,6 +42,7 @@ class Blob():
         if self.pos[1] > self.windowSize[1]-self.blobSize:
             self.pos[1] = self.windowSize[1]-self.blobSize
             obstacle = True
+            self.onGround = True
         if not obstacle:
             self.pos[1] += y
             return True
@@ -56,6 +62,36 @@ class Blob():
             self.fallVel += self.fallVel*0.1
         else:
             self.fallVel = 1
+
+class Block():
+    def __init__(self, windowSize):
+        self.windowSize = windowSize
+        self.blockSize = windowSize[1]//7
+        self.blockImg = pygame.transform.scale(pygame.image.load("leblocksingle.png"), (self.blockSize, self.blockSize))
+        self.pos = [windowSize[0], windowSize[1]-(self.blockSize*random.randint(1,2))]
+
+    def getPos(self):
+        return self.pos
+
+    def getSize(self):
+        return self.blockSize
+    
+    def move(self, x):
+        self.pos[0] += x
+
+    def draw(self, screen):
+        screen.blit(self.blockImg, self.pos)
+
+def collision(Pos1, Size1, Pos2, Size2):
+    if Pos1[0]+Size1 >= Pos2[0]:
+        if Pos1[1]+Size1 > Pos2[1] and Pos1[1] < Pos2[1]+Size2:
+            return 0
+    elif Pos1[1]+Size1 >= Pos2[1]:
+        if Pos1[0]+Size1 > Pos2[0] and Pos1[0] < Pos2[0]+Size2:
+            print("on block")
+            return 1
+    else:
+        return 2
             
 
 def game():
@@ -69,9 +105,12 @@ def game():
     count = 0
     jumpLimit = 2
     jumpCount = 0
+    blocks = []
 
     changeDrawing = pygame.USEREVENT
     pygame.time.set_timer(changeDrawing, 500)
+    spawnBlock = pygame.USEREVENT
+    pygame.time.set_timer(spawnBlock, 500)
 
     screen = pygame.display.set_mode(size)
     
@@ -86,14 +125,31 @@ def game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+
             if event.type == changeDrawing:
                 blob.changeDraw()
+
+            if event.type == spawnBlock:
+                luck = random.randint(0, 4)
+                if not luck:
+                    blocks.append(Block(size))
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
-                    jumpCount += 1
-                    blob.jumping = True
+                    if not jumpCount >= jumpLimit:
+                        jumpCount += 1
+                        blob.jumping = True
     
         # --- Game logic should go here
+
+        # collision 
+        for block in blocks:
+            if collision(blob.getPos(), blob.getSize(), block.getPos(), block.getSize()) == 1:
+                blob.collision = 1
+            elif collision(blob.getPos(), blob.getSize(), block.getPos(), block.getSize()) == 0:
+                print("game over")
+            elif collision(blob.getPos(), blob.getSize(), block.getPos(), block.getSize()) == 2:
+                blob.collision = 0
 
         # moving background
         for x in range(len(bg)):
@@ -102,13 +158,18 @@ def game():
             bg[0] = 0
             bg[1] = size[0]
 
+        # moving blob, really just jumping and falling
         if not blob.jumping:
             blob.fall()
         else:
-            #if jumpCount <= jumpLimit:
-            print(jumpCount, jumpLimit)
             blob.fallVel = 1
             blob.jump()
+        if blob.onGround:
+            jumpCount = 0
+
+        # moving blocks
+        for block in blocks:
+            block.move(-5)
     
         # --- Screen-clearing code goes here
         for x in bg:
@@ -116,6 +177,8 @@ def game():
     
         # --- Drawing code should go here
         blob.draw(screen)
+        for block in blocks:
+            block.draw(screen)
     
         # --- Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
